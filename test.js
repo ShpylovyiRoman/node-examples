3ea11b8eb1ba106a74f9dfd701c39fb5cb0105bb
Aezakmi <aezakmi@localhost.localdomain>
2018-11-12 01:49:40 +0200

    test

--- test.js
+++ test.js
@@ -1,11 +1,25 @@
 'use strict';
 
-const fs = require('fs');
-const translate = require('google-translate-api');
+const x1 = 3, y1 = 5, r1 = 5;
+const x2 = 4, y2 = 8, r2 = 7;
 
-const eng = fs.readFileSync('/home/aezakmi/eng.txt', 'utf8');
-console.log(eng);
-//not working...
-translate('Hello world', { from: 'en', to: 'uk' })
-  .then(ru => { console.log(ru.text); })
-  .catch(err => { console.error(err); });
+const x3 = x2 - x1;
+const y3 = y2 - y1;
+
+const sum = Math.sqrt(Math.pow(x3, 2) + Math.pow(y3, 2));
+
+const sumr = r1 + r2;
+
+if (x1 === x2, y1 === y2, r1 === r2) {
+  console.log('бесконечное множество точок пересичения');
+}
+
+if (sum < sumr) {
+  console.log('2 точки пересичения');
+}
+if (sum > sumr) {
+  console.log('точок пересичения нет');
+}
+if (sum === sumr) {
+  console.log('1 точка пересичения');
+}
